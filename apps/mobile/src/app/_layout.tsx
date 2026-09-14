86d2ee21203e5b0f826da7302fcde2650817ba3d
Ariel Poblete DES <apoblete.des@gmail.com>
2026-09-14 14:36:51 -0300

    expo app is full!!!!

---
+++ apps/mobile/src/app/_layout.tsx
@@ -0,0 +1,17 @@
+import * as SplashScreen from "expo-splash-screen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/components/app-tabs";
+
+const queryClient = new QueryClient();
+
+SplashScreen.preventAutoHideAsync();
+
+export default function TabLayout() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+    </QueryClientProvider>
+  );
+}
