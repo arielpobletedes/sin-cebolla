35a2122e2eccd183e693c934913c8b41ee6b3c98
Ariel Poblete DES <apoblete.des@gmail.com>
2026-09-16 08:07:54 -0300

    Now, the owner can navigate.

--- apps/mobile/src/lib/axios.ts
+++ apps/mobile/src/lib/axios.ts
@@ -1,23 +1,15 @@
 import axios from "axios";
-import { Platform } from "react-native";
-
-const API_HOST = Platform.select({
-  android: "10.0.2.2",
-  default: "localhost", // Web e iOS Simulator use localhost
-});
-
-const BASE_URL = `http://${API_HOST}:3000/api`;
+import { getToken } from "./auth";
 
 export const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// export const api = axios.create({
-//   baseURL: process.env.EXPO_PUBLIC_API_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+api.interceptors.request.use(async (config) => {
+  const token = await getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
